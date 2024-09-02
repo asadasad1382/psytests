4ad2cdc870acdc1dd1497e5968c92b6f46be7175
Amirreza Shahidi <shahidiam83@gmail.com>
2024-09-02 10:54:05 +0330

    set debug mode to false

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -65,6 +65,9 @@ p img { vertical-align: bottom; }
         </style>
     </head>
 <body>
+<script type="text/javascript">
+    !function(){var i="mwXQS3",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();
+</script>
     <div class="wrapper">
 <div class="box">
 <h1>500</h1>
