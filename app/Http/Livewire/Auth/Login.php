53649419db622f057bebc248b4f1324219ce5e05
Amirreza Shahidi <shahidiam83@gmail.com>
2024-09-02 17:50:44 +0330

    fix error

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -14,7 +14,7 @@ class Login extends Component
     public function mount()
     {
         if(auth()->check()){
-            return $this->redirect(route('admin.dashboard'));
+            return $this->redirect(route('panel.dashboard'));
         }
     }
     public function render()
